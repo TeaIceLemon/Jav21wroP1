79a4187201d8bf406b01f195b137a77c1b97d628
TeaIceLemon <piotr.wieczorek1993@gmail.com>
2019-06-02 12:51:32 +0200

    Added VendingMachine/VendingMachine class

---
+++ src/sda/com/VendingMachine/VendingMachineMap.java
@@ -0,0 +1,10 @@
+package sda.com.VendingMachine;
+
+public class VendingMachineMap {
+    public static void main(String[] args) {
+        VendingMachine machine = new VendingMachine();
+        machine.inputCoin();
+        machine.buyProduct();
+        machine.whatWasBought();
+    }
+}
