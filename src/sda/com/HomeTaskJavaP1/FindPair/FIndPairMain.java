7b2443ab433b95324b1d7f09ec7960a0c96f5368
TeaIceLemon <piotr.wieczorek1993@gmail.com>
2019-06-06 00:43:17 +0200

    Added FindPairOptimal method to FindPair/FindPairUtils ;

--- src/sda/com/HomeTaskJavaP1/FindPair/FIndPairMain.java
+++ src/sda/com/HomeTaskJavaP1/FindPair/FIndPairMain.java
@@ -1,9 +1,19 @@
 package sda.com.HomeTaskJavaP1.FindPair;
 
+import sda.com.StopWatch.StopWatch;
+
 public class FIndPairMain {
     public static void main(String[] args) {
+        StopWatch watch = new StopWatch();
         FindPairUtils findPair = new FindPairUtils();
         int[] arr = {1,4,6,8,5,3};
-        findPair.FindPairBruteForce(arr,9);
+//        System.out.println("Brute Force");
+//        watch.start();
+//        findPair.findPairBruteForce(arr,9);
+//        System.out.println(watch.stop());
+        System.out.println("Optimal");
+        watch.start();
+        findPair.findPairOptimal(arr,9);
+        System.out.println(watch.stop());
     }
 }
--- src/sda/com/HomeTaskJavaP1/FindPair/FindPairUtils.java
+++ src/sda/com/HomeTaskJavaP1/FindPair/FindPairUtils.java
@@ -3,7 +3,7 @@ package sda.com.HomeTaskJavaP1.FindPair;
 import sda.com.HomeTaskJavaP1.InsertionSort.InsertionSort;
 
 public class FindPairUtils {
-    public void FindPairBruteForce(int[] arr, int sum){
+    public void findPairBruteForce(int[] arr, int sum){
         boolean isPairFound = false;
         for(int i =0; i<arr.length-1;i++) {  // i-> first Pair element
             for(int j = i+1; j<arr.length;j++){ //j -> second element of Pair
@@ -19,6 +19,22 @@ public class FindPairUtils {
         }
 
     }
+    public void findPairOptimal(int[] arr, int sum){
+        boolean isPairFound = false;
+        InsertionSort.insertioSort(arr); //sort array
+        int low = 0; // fist element of array
+        int higt = arr.length -1; //last element of array
+        while(low<higt){
+            if(arr[low] + arr[higt] == sum){
+                isPairFound = true;
+                System.out.println("Pair fount at " + low + " and " + higt);
+                System.out.println();
+            }
+            if(arr[low] + arr[higt] > sum) --higt;
+            else ++low;
+        }
+        if(isPairFound==false) System.out.println("No pair found");
 
+    }
 
 }
